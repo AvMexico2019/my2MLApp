b1d4333c7178be144f88617313ec9606d3f2e408
AvMexico2019 <56038497+AvMexico2019@users.noreply.github.com>
2019-10-09 18:20:00 -0500

    Add project files.

---
+++ my2MLApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my2MLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
